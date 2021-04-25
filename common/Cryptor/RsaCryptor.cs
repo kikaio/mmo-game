f4a1324f493efd00cb5a03cc172db203963163b0
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-25 17:40:47 +0900

    RSA방식 암호, 복호화 코드 추가.

---
+++ common/Cryptor/RsaCryptor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Cryptor
+{
+    internal class RsaCryptor
+    {
+        public static string RsaEncryptWithBase64(string _base64, RSAParameters _publicParam)
+        {
+            using (var _rsa = new RSACryptoServiceProvider())
+            {
+                byte[] _data = Convert.FromBase64String(_base64);
+                _rsa.ImportParameters(_publicParam);
+                byte[] encrypted = _rsa.Encrypt(_data, false);
+                return Convert.ToBase64String(encrypted);
+            }
+        }
+        public static string RsaDecryptWithBase64(string _base64, RSAParameters _privateParam)
+        {
+            using (var _rsa = new RSACryptoServiceProvider())
+            {
+                _rsa.ImportParameters(_privateParam);
+                byte[] _data = Convert.FromBase64String(_base64);
+                byte[] decBytes = _rsa.Decrypt(_data, false);
+                return Convert.ToBase64String(decBytes);
+            }
+        }
+    }
+}
