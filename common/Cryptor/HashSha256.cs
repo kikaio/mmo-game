f4cb68dcdb2897829d88a5e51f1147673a296dd6
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-25 17:46:49 +0900

    단방향 해시 Sha256 추가.

---
+++ common/Cryptor/HashSha256.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Cryptor
+{
+    internal class HashSha256
+    {
+        public static string PlainStrToBase64WithSha256(string _plainStr, string _saltkey)
+        {
+            var sb = new StringBuilder(_plainStr);
+            sb.Append(_saltkey);
+            var shaMgr = new SHA256Managed();
+            var encBytes = shaMgr.ComputeHash(Encoding.UTF8.GetBytes(sb.ToString()));
+            return Convert.ToBase64String(encBytes);
+        }
+    }
+}
