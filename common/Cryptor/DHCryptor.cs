f7d670cfb27f76c28c4f7a380512dc09ad630f9b
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-25 17:58:24 +0900

    Dh 암호화 코드 추가.

---
+++ common/Cryptor/DHCryptor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Cryptor
+{
+    internal class DHCryptor
+    {
+        public static byte[] DhEncrypt(string _msgBase64, byte[] _key, byte[] _iv)
+        {
+            var ret = default(byte[]);
+            using (var aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.IV = _iv;
+                ICryptoTransform enc = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, enc, CryptoStreamMode.Write))
+                    {
+                        using (var sw = new StreamWriter(cs))
+                        {
+                            sw.Write(_msgBase64);
+                        }
+                    }
+                    ret = ms.ToArray();
+                }
+            }
+            return ret;
+        }
+
+        public static string DhDecrypt(byte[] _bytes, byte[] _key, byte[] _iv)
+        {
+            var ret = default(string);
+            using (var aes = Aes.Create())
+            {
+                aes.Key = _key;
+                aes.IV = _iv;
+                ICryptoTransform dec = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (var ms = new MemoryStream(_bytes))
+                {
+                    using (var cs = new CryptoStream(ms, dec, CryptoStreamMode.Read))
+                    {
+                        using (var sr = new StreamReader(cs))
+                        {
+                            ret = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return ret;
+        }
+    }
+}
