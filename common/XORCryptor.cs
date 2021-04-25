82cb53696bb38648470db0ae78a0d6c67069b216
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-25 17:34:51 +0900

    no message

---
+++ common/XORCryptor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Crypt
+{
+    internal class XORCryptor
+    {
+        public static byte[] ApplyZor(byte[] _origin, byte[] _keyBytes)
+        {
+            var ret = new byte[_origin.Length];
+            Array.Copy(_origin, ret, _origin.Length);
+
+            int idx = 0;
+
+            while (idx < _origin.Length)
+            {
+                foreach (var keyEle in _keyBytes)
+                {
+                    ret[idx] ^= keyEle;
+                    idx++;
+                    if (idx == _origin.Length)
+                        break;
+                }
+            }
+            return ret;
+        }
+    }
+}
