5056ac2dbc290c7f7bb5949cba2fb85b3ee8e7a2
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-25 18:16:12 +0900

    logger용 코드 추가.

---
+++ common/Utils/Loggers/CoreLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Utils.Loggers
+{
+    public abstract class CoreLogger : IDisposable
+    {
+        public CoreLogger()
+        { }
+
+        public virtual void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract void Write(string _log);
+
+        public abstract void WriteTrace(string _log, [CallerMemberName]string _mName = "", [CallerLineNumber]int _line = 0);
+
+        public abstract void Error(string _log);
+
+        [Conditional("DEBUG")]
+        public abstract void WriteDebug(string _log = "");
+
+        [Conditional("DEBUG")]
+        public abstract void WriteDebugTrace(string _log = "", [CallerMemberName]string _mName = "", [CallerLineNumber]int _line = 0);
+
+        [Conditional("DEBUG")]
+        public abstract void WriteDebugWarn(string _log = "");
+
+        [Conditional("DEBUG")]
+        public abstract void WriteDebugError(string _log = "");
+    }
+}
