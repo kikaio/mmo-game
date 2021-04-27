b92cc2264f9c00266347072433864b516fe911a9
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-27 18:14:42 +0900

    Packet 클래스 추가.

---
+++ common/Protocols/Packet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Protocols
+{
+    public class Packet
+    {
+        public static int GetHeaderSize()
+        {
+            return sizeof(Int32);
+        }
+
+        public enum PACKET_TYPE : ushort
+        {
+            NONE,
+            NOTI,
+            REQ,
+            ANS,
+            TEST,
+            END,
+        }
+
+
+
+    }
+}
