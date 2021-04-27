1025e5c53a74c3da033569195892e14b1704e3f9
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-27 19:07:52 +0900

    Packet, Session 포함하는 Package 클래스 추가.

---
+++ common/Protocols/Package.cs
@@ -0,0 +1,21 @@
+﻿using common.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Protocols
+{
+    public class Package
+    {
+        public CoreSession session { get; private set; } = null;
+        public Packet Packet { get; private set; } = null;
+
+        public Package(CoreSession _s, Packet _p)
+        {
+            session = _s;
+            Packet = _p;
+        }
+    }
+}
