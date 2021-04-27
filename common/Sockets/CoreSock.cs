70d4346b9d324465930907af9c1c001e36d3027d
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-27 18:54:28 +0900

    Socket용 wrapping 클래스 추가.

---
+++ common/Sockets/CoreSock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Sockets
+{
+    public abstract class CoreSock
+    {
+        public Socket Sock;
+        public int Port;
+        public IPAddress Addr;
+    }
+
+    public class CoreTCP : CoreSock
+    {
+        public CoreTCP(Socket _sock)
+        {
+            Sock = _sock;
+        }
+
+        public CoreTCP(AddressFamily _af = AddressFamily.InterNetwork)
+        {
+            Sock = new Socket(_af, SocketType.Stream, ProtocolType.Tcp);
+        }
+    }
+
+    public class CoreUDP : CoreSock
+    {
+        public CoreUDP(AddressFamily _af = AddressFamily.InterNetwork)
+        {
+            Sock = new Socket(_af, SocketType.Dgram, ProtocolType.IP);
+        }
+    }
+}
