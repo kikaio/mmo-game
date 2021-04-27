308e233e2958f16da1d5531329781c5f4a32ee46
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-27 18:27:42 +0900

    Packet 구현부 추가.

--- common/Protocols/Packet.cs
+++ common/Protocols/Packet.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using common.Networking;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +25,84 @@ namespace common.Protocols
             END,
         }
 
+        public NetStream header { get; protected set; } = new NetStream(GetHeaderSize());
+        public NetStream data { get; protected set; }
+        public PACKET_TYPE pType { get; protected set; } = PACKET_TYPE.NONE;
 
+        public Packet(long _capacity = 512)
+        {
+            if (_capacity <= 0)
+                data = null;
+            else
+                data = new NetStream(_capacity);
+        }
 
+        protected Packet(Packet _p)
+        {
+            header = _p.header;
+            data = _p.data;
+            pType = _p.pType;
+        }
+
+        protected Packet(NetStream _h, NetStream _d)
+        {
+            header = _h;
+            data = _d;
+        }
+
+        public void SetHeader(Int32 _val)
+        {
+            header.ResetOffset();
+            header.WriteInt32(_val);
+        }
+
+        public void UpdateHeader()
+        {
+            if (data == null)
+                SetHeader(0);
+            else
+                SetHeader(data.offset);
+        }
+
+        public Int32 GetHeader()
+        {
+            header.ResetOffset();
+            return header.ReadInt32();
+        }
+
+        protected void ClearData()
+        {
+            if (data == null)
+                return;
+            Array.Clear(data.bytes, 0, data.bytes.Length);
+            data.ResetOffset();
+        }
+
+        [Conditional("DEBUG")]
+        public void RenderPacket(string _header = "")
+        {
+            Console.WriteLine($"Pacekt========{_header}");
+            header?.RenderBytes();
+            data?.RenderBytes();
+            Console.WriteLine($"================");
+        }
+        [Conditional("DEBUG")]
+        public void RenderProperties()
+        {
+            StringBuilder sb = new StringBuilder("[RenderProperties]");
+            var pList = GetType().GetProperties();
+            foreach (var pInfo in pList)
+            {
+                if (pInfo.CanWrite == false)
+                    continue;
+                sb.AppendLine($"{pInfo.Name}:{pInfo.GetValue(this).ToString()}");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        public void ReadPacketType()
+        {
+            pType = (PACKET_TYPE)data.ReadUInt16();
+        }
     }
 }
