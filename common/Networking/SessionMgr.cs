7ac98453752a5deaf24e58af00984656fcd72353
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-28 16:59:06 +0900

    SessionMgr 추가.

---
+++ common/Networking/SessionMgr.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace common.Networking
+{
+    using SessionDict = ConcurrentDictionary<long, CoreSession>;
+    public class SessionMgr
+    {
+        public static SessionMgr Inst { get; private set; } = new SessionMgr();
+        SessionDict sessionDict = new SessionDict();
+
+        long curSessionId = 0;
+
+        public bool AddSession(CoreSession _s)
+        {
+            var oldSession = default(CoreSession);
+            if (CloseSession(_s.SessionId, out oldSession))
+            {
+                // 이경우는 뭘까.?
+            }
+            if (sessionDict.TryAdd(_s.SessionId, _s))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void RemoveAllSession()
+        {
+            sessionDict.Clear();
+        }
+        public void CloseAllSession()
+        {
+            foreach (var ele in ToSessonList())
+            {
+                var sock = ele.Sock.Sock;
+                ele.DoDispose(true);
+                sock.Disconnect(true);
+                sock.Close();
+            }
+            sessionDict.Clear();
+        }
+        public bool CloseSession(long _id, out CoreSession _s)
+        {
+            _s = default(CoreSession);
+            if (sessionDict.TryRemove(_id, out _s))
+            {
+                _s.DoDispose(true);
+                _s.Sock.Sock.Disconnect(true);
+                _s.Sock.Sock.Close();
+                return true;
+            }
+            return false;
+        }
+
+        public bool ForceCloseSession(long _id, out CoreSession _s)
+        {
+            _s = default(CoreSession);
+            if (sessionDict.TryRemove(_id, out _s))
+            {
+                _s.DoDispose(true);
+                _s.Sock.Sock.Shutdown(SocketShutdown.Both);
+                _s.Sock.Sock.Close();
+
+                return true;
+            }
+            return false;
+        }
+
+        public bool GetSession(long _id, out CoreSession _s)
+        {
+            _s = default(CoreSession);
+            if (sessionDict.TryGetValue(_id, out _s))
+                return true;
+            return false;
+        }
+
+        public long GetNextSessionId()
+        {
+            return ++curSessionId;
+        }
+
+        public List<CoreSession> ToSessonList()
+        {
+            return sessionDict.Values.ToList();
+        }
+
+    }
+}
