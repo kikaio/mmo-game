31871050e05cb81ca8f3ca5ec570a3f48bbdb598
yonwook heo <gjduddnr5923@naver.com>
2021-04-28 22:31:10 +0900

    기본 뼈대 코드 추가.

--- common/Samples/SampleClient.cs
+++ common/Samples/SampleClient.cs
@@ -1,13 +1,112 @@
-﻿using System;
+﻿using common.Networking;
+using common.Protocols;
+using common.Sockets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace common.Samples
 {
-    public class SampleClient
+    //Sync, Tcp 기반 샘플용 client
+    public class SampleClient : CoreNetwork
     {
+        private CoreSession mSession = default(CoreSession);
 
+        public SampleClient(string _host, int _port)
+        {
+            ep = new IPEndPoint(IPAddress.Parse(_host), _port);
+        }
+        public override void ReadyToStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConnectToServer()
+        {
+            var sock = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            sock.Connect(ep);
+            mSession = new CoreSession(-1, new CoreTCP(sock));
+        }
+
+        public override void Start()
+        {
+            ConnectToServer();
+        }
+
+        protected override void Analizer_Ans(CoreSession _s, Packet _p)
+        {
+            var sp = new SamplePacket(_p);
+            switch (sp.cType)
+            {
+                case CONTENT_TYPE.NONE:
+                    break;
+                case CONTENT_TYPE.HB_CHECK:
+                    break;
+                case CONTENT_TYPE.TEST:
+                    break;
+                case CONTENT_TYPE.END:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected override void Analizer_Noti(CoreSession _s, Packet _p)
+        {
+            var sp = new SamplePacket(_p);
+            switch (sp.cType)
+            {
+                case CONTENT_TYPE.NONE:
+                    break;
+                case CONTENT_TYPE.HB_CHECK:
+                    break;
+                case CONTENT_TYPE.TEST:
+                    break;
+                case CONTENT_TYPE.END:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected override void Analizer_Req(CoreSession _s, Packet _p)
+        {
+            var sp = new SamplePacket(_p);
+            switch (sp.cType)
+            {
+                case CONTENT_TYPE.NONE:
+                    break;
+                case CONTENT_TYPE.HB_CHECK:
+                    break;
+                case CONTENT_TYPE.TEST:
+                    break;
+                case CONTENT_TYPE.END:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected override void Analizer_Test(CoreSession _s, Packet _p)
+        {
+            var sp = new SamplePacket(_p);
+            switch (sp.cType)
+            {
+                case CONTENT_TYPE.NONE:
+                    break;
+                case CONTENT_TYPE.HB_CHECK:
+                    break;
+                case CONTENT_TYPE.TEST:
+                    break;
+                case CONTENT_TYPE.END:
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
