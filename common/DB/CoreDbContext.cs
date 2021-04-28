fc9d432903e8683acc29ef6becdfcd60eb9d40b0
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-28 17:22:30 +0900

    DbContext 코드 추가.

--- common/DB/CoreDbContext.cs
+++ common/DB/CoreDbContext.cs
@@ -1,12 +1,81 @@
-﻿using System;
+﻿using MySql.Data.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace common.DB
 {
-    public class CoreDbContext
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class CoreDbContext : DbContext
     {
+        protected CoreDbContext(string _connStr)
+            : base(_connStr)
+        {
+
+        }
+
+        public virtual void DoActAndSave(Action _act)
+        {
+            using (var conn = this.Database.Connection)
+            {
+                conn.Open();
+                DoWithTransaction(() => {
+                    _act();
+                    SaveChanges();
+                });
+            }
+        }
+        //음 되는건가???
+        public virtual async Task DoActAndSaveAsync(Action _act)
+        {
+            using (var conn = this.Database.Connection)
+            {
+                await conn.OpenAsync();
+                if (_act != null)
+                {
+                    await DoAsyncWithTransaction(async () =>
+                    {
+                        await Task.Factory.StartNew(_act);
+                        await SaveChangesAsync();
+                    });
+                }
+            }
+        }
+
+        //단지 transaction을 감싸줄뿐 context에 대한 save등은 직접 호출해야함.
+        private void DoWithTransaction(Action _act)
+        {
+            try
+            {
+                using (var tr = this.Database.BeginTransaction())
+                {
+                    _act?.Invoke();
+                    tr.Commit();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private async Task DoAsyncWithTransaction(Action _act)
+        {
+            try
+            {
+                using (var tr = Database.BeginTransaction())
+                {
+                    await Task.Factory.StartNew(_act);
+                    tr.Commit();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
