ffeac1ee9af75b396d279b58d6e9498555bd3ecc
yonwook heo <gjduddnr5923@naver.com>
2021-04-28 23:18:42 +0900

    초간단 Room 클래스 추가.

---
+++ server/Area/Room.cs
@@ -0,0 +1,30 @@
+﻿using server.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Area
+{
+    using RoomId = Int32;
+    using UserId = Int64;
+    public class Room
+    {
+        public RoomId mRoomId { get; private set; }
+        private Dictionary<UserId, User> mUserDict = new Dictionary<UserId, User>();
+        public Room(RoomId _id)
+        {
+            mRoomId = _id;
+        }
+
+        public void JoinUser(User _user)
+        { 
+        }
+
+        public void QuitUser(UserId _uid)
+        {
+
+        }
+    }
+}
