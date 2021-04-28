b5b95eaceee561dbace6f1b0100b6657210b99e6
yonwook heo <gjduddnr5923@naver.com>
2021-04-28 23:19:12 +0900

    기본 Server 클래스 추가.

---
+++ server/MmoServer.cs
@@ -0,0 +1,44 @@
+﻿using common.Networking;
+using common.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    public class MmoServer : CoreNetwork
+    {
+        //음.....우선 단일 로비홀 -> 룸 으로 할까.
+        public override void ReadyToStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Analizer_Ans(CoreSession _s, Packet _p)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Analizer_Noti(CoreSession _s, Packet _p)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Analizer_Req(CoreSession _s, Packet _p)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Analizer_Test(CoreSession _s, Packet _p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
