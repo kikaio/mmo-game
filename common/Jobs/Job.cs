90331893f42478dc94d88642dbfd7581028444da
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-04-28 17:22:44 +0900

    Job 클래스 추가.

---
+++ common/Jobs/Job.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Jobs
+{
+    public abstract class Job
+    {
+        //0 : 단 1번 실행. 0 > val : sec지날때마다, 0 < val : 무한반복.
+        public DateTime EndDate { get; protected set; } = DateTime.MinValue;
+        public DateTime StartDate { get; protected set; } = DateTime.MinValue;
+        protected long deltaTick;
+        protected Action JobAct = null;
+        protected Task JobTask = null;
+
+        public Job()
+        {
+        }
+
+        public abstract bool Tick();
+        //딱 한번만 실행.
+        public virtual async Task<bool> TickAsync()
+        {
+            throw new NotImplementedException("");
+        }
+    }
+}
