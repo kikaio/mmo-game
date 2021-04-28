e38a9d8ea2576e3a230fc4223381c03af2852024
yonwook heo <gjduddnr5923@naver.com>
2021-04-28 23:35:21 +0900

    유저 입장 및 퇴장 관련 코드 추가.

--- server/Area/Room.cs
+++ server/Area/Room.cs
@@ -1,4 +1,5 @@
-﻿using server.Users;
+﻿using common.Utils.Loggers;
+using server.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,18 +14,46 @@ namespace server.Area
     {
         public RoomId mRoomId { get; private set; }
         private Dictionary<UserId, User> mUserDict = new Dictionary<UserId, User>();
+        private CoreLogger logger = new ConsoleLogger();
+
+        public static int MaxUserCnt = 10;
+        private int curUserCnt = 0;
         public Room(RoomId _id)
         {
             mRoomId = _id;
         }
 
-        public void JoinUser(User _user)
-        { 
+        public bool JoinUser(User _user)
+        {
+            if (curUserCnt == MaxUserCnt)
+            {
+                logger.WriteDebugWarn($"this room[{mRoomId}] is full");
+                return false;
+            }
+            if (mUserDict.ContainsKey(_user.uId))
+            {
+                logger.WriteDebugWarn($"this user[{_user.uId}] already exit in this room[{mRoomId}]");
+                return false;
+            }
+            mUserDict.Add(_user.uId, _user);
+            logger.WriteDebug($"this user[{_user.uId}] join this room[{mRoomId}]");
+            curUserCnt++;
+            return true;
         }
 
-        public void QuitUser(UserId _uid)
+        public bool QuitUser(UserId _uid, out User _quitUser)
         {
-
+            _quitUser = default(User);
+            if (curUserCnt == 0)
+            {
+                logger.Error($"room[{mRoomId}] is something wrong");
+                return false;
+            }
+            if (mUserDict.ContainsKey(_uid) == false)
+                return false;
+            _quitUser = mUserDict[_uid];
+            curUserCnt--;
+            return true;
         }
     }
 }
