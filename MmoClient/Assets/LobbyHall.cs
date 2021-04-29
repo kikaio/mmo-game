acb1f42d5f5ad19e2b0e91b0dea602becfea7eb5
yonwook heo <gjduddnr5923@naver.com>
2021-04-29 23:54:25 +0900

    로비홀 객체에 dll 참조 추가.

--- MmoClient/Assets/LobbyHall.cs
+++ MmoClient/Assets/LobbyHall.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using clientForUnity;
+
 public class LobbyHall : MonoBehaviour
 {
     // Start is called before the first frame update
