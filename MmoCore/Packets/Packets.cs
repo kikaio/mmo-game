f7529afe9b0dfea19a4bf3adeb906cd7b5e0f61d
yonwook heo <gjduddnr5923@naver.com>
2021-04-29 21:11:13 +0900

    Mmo용 Packet 추가.

---
+++ MmoCore/Packets/Packets.cs
@@ -0,0 +1,57 @@
+﻿using common.Protocols;
+using MmoCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MmoCore.Packets
+{
+    public class MmoCorePacket : Packet
+    {
+        public CONTENT_TYPE cType { get; set; } = CONTENT_TYPE.NONE;
+
+        public MmoCorePacket(Packet _p) : base(_p)
+        {
+            var ret = _p as MmoCorePacket;
+            if (ret != default(MmoCorePacket))
+                cType = ret.cType;
+        }
+
+        public override void PacketRead()
+        {
+            base.PacketRead();
+            cType = (CONTENT_TYPE)Translate.Read<ushort>(data);
+        }
+
+        public override void PacketWrite()
+        {
+            base.PacketWrite();
+            Translate.Write(data, (ushort)cType);
+        }
+    }
+
+
+
+    public class WelcomePacket : MmoCorePacket
+    {
+        public long sId { get; set; }
+
+        public WelcomePacket(MmoCorePacket _p) : base(_p)
+        {
+        }
+
+        public override void PacketRead()
+        {
+            base.PacketRead();
+            sId = Translate.Read<long>(data);
+        }
+
+        public override void PacketWrite()
+        {
+            base.PacketWrite();
+            Translate.Write(data, sId);
+        }
+    }
+}
