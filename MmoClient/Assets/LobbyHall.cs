31527b2821fe845287389d8847716083bb46d327
yonwook heo <gjduddnr5923@naver.com>
2021-04-30 00:02:08 +0900

    유니티 저장.

--- MmoClient/Assets/LobbyHall.cs
+++ MmoClient/Assets/LobbyHall.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using clientForUnity;
+using common;
+using MmoCore;
 
 public class LobbyHall : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
