a39fec29344ffbe8ef832e824483ea35cac0f657
yonwook heo <gjduddnr5923@naver.com>
2021-04-29 23:53:54 +0900

    client 용 User 클래스 추가. -> Unity에서 활용 시 반드시 빌드 후 dll을 plugins에 넣어줘야함.

---
+++ clientForUnity/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace clientForUnity
+{
+    public class User
+    {
+        public string name;
+        public long sId;
+        public Vector3 color = new Vector3(0, 0, 0);
+    }
+}
