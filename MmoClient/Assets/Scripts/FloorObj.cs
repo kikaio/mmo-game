d75512bb08d5452bd47bb3f4eea570794b72a739
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-05-01 17:18:14 +0900

    객체 cs 추가

---
+++ MmoClient/Assets/Scripts/FloorObj.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorObj : MonoBehaviour
+{
+    public Sprite lobbyHallSprite;
+
+    public List<Sprite> floorCubeList;
+
+    
+    public 
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ MmoClient/Assets/Scripts/LobbyHall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using clientForUnity;
+using common;
+using MmoCore;
+
+public class LobbyHall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
