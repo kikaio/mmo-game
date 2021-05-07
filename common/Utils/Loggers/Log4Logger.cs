0022ce19611f57f131acc737a8451809446a3cfb
gjdud <gjdud@DESKTOP-LJ2MCJH>
2021-05-07 15:38:56 +0900

    common에 log4net lib 참조 및 활용 logger 추가.

---
+++ common/Utils/Loggers/Log4Logger.cs
@@ -0,0 +1,51 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace common.Utils.Loggers
+{
+    public class Log4Logger : CoreLogger
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(Log4Logger));
+
+        public override void Error(string _log)
+        {
+            logger.Error(_log);
+        }
+
+        public override void Write(string _log)
+        {
+            logger.Info(_log);
+        }
+
+        public override void WriteDebug(string _log = "")
+        {
+            logger.Debug(_log);
+        }
+
+        public override void WriteDebugError(string _log = "")
+        {
+            logger.Error(_log);
+        }
+
+        public override void WriteDebugTrace(string _log = "", [CallerMemberName] string _mName = "", [CallerLineNumber] int _line = 0)
+        {
+            logger.Debug($"[{_mName}::{_line}]{_log}");
+        }
+
+        public override void WriteDebugWarn(string _log = "")
+        {
+            logger.Warn(_log);
+        }
+
+        public override void WriteTrace(string _log, [CallerMemberName] string _mName = "", [CallerLineNumber] int _line = 0)
+        {
+            logger.Warn($"[{_mName}::{_line}]{_log}");
+        }
+    }
+}
