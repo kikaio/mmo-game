ceb930a217aa07a7e11da7389e4e7b7e8cace79d
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-22 18:02:44 +0900

    RPCMgr 작업중.

---
+++ common/Protocols/CoreRPCMgr.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Protocols
+{
+    //int : errorcode
+    using RPCWrapper = Func<List<object>, int>;
+    using RPCMap = Dictionary<string, RPCWrapper>;
+    using RPCCallTypeToMap = Dictionary<CoreRPCMgr.ECALL_TYPE, RPCMap>;
+    using EntityRPCMap = Dictionary<Type, RPCTypeToMap>;
+
+    public class CoreRPCMgr
+    {
+        public enum ECALL_TYPE
+        {
+            REQ,
+            ANS,
+            NOTI,
+            TEST,
+        };
+
+        private static CoreRPCMgr inst;
+        public static CoreRPCMgr Inst
+        {
+            get {
+                if (inst == null)
+                    inst = new CoreRPCMgr();
+                return inst;
+            }
+        }
+    }
+
+}
