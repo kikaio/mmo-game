ea7a5fae278c857d04e2c1d0ed1779c0605423ae
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-27 16:22:07 +0900

    단 한번만 호출하는 job 추가.

---
+++ common/Jobs/JobOnce.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace common.Jobs
+{
+    public class JobOnce : Job
+    {
+        public JobOnce(DateTime _sd)
+        {
+            StartDate = _sd;
+        }
+
+        //Do Job once after start datetime, and then return false;
+        public override bool Tick()
+        {
+            DateTime nowUtc = DateTime.UtcNow;
+            if (StartDate > nowUtc)
+                return true;
+            JobAct?.Invoke();
+            return false;
+        }
+
+        public override async Task<bool> TickAsync()
+        {
+            return false;
+        }
+    }
+}
